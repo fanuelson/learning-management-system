df7e269babb0e31fb55462094e53ab4eef140672
Fanuel Nunes <fnunes@testreach.com>
2025-05-08 14:08:43 -0300

    fixes

--- frontend/src/app/feature/curso/curso.routes.ts
+++ frontend/src/app/feature/curso/curso.routes.ts
@@ -4,7 +4,6 @@ import { CursoDetailsResolver } from './curso.resolver';
 import { CursoFormComponent } from './curso-form/curso-form.component';
 import { CursoListComponent } from './curso-list/curso-list.component';
 import { CursoDetailsComponent } from './curso-details/curso-details.component';
-import { PermissionGuard } from '@core/guards/permission.guard';
 
 export default [
   {
@@ -15,24 +14,20 @@ export default [
       {
         path: '',
         component: CursoListComponent,
-        canActivate: [PermissionGuard('ADMIN')]
       },
       {
         path: 'create',
         component: CursoFormComponent,
-        canActivate: [PermissionGuard('ADMIN')]
       },
       {
         path: 'edit/:cursoId',
         component: CursoFormComponent,
-        canActivate: [PermissionGuard('ADMIN')],
         resolve: { cursoDetails: CursoDetailsResolver },
         
       },
       {
         path: 'details/:cursoId',
         component: CursoDetailsComponent,
-        canActivate: [PermissionGuard('ADMIN')],
         resolve: { cursoDetails: CursoDetailsResolver }
       }
     ]
